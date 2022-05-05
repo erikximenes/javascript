9c92781c01b1f9ae7b70cb6d2a7b8a27dccb558c
Erik Ximenes <101354712+erikximenes@users.noreply.github.com>
2022-05-05 14:47:29 -0300

    Exercícios

---
+++ aula11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO !!!`)
+}
+console.log(`Digija sempre usando cinto de segurança!`)
